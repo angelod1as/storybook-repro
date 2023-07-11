873f22ad8d25ac331a7831eb7b62d3a010d2a1e9
Angelo Dias <angelo@foobar.agency>
2023-07-11 09:53:08 +0200

    Remove preset-scss

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,30 +1,21 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 import path from "path"
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-scss",
   ],
   framework: {
     name: "@storybook/nextjs",
-    options: {},
+    options: {
+      nextConfigPath: path.resolve(__dirname, "../next.config.js"),
+    },
   },
   docs: {
     autodocs: "tag",
-  },
-  webpackFinal: async (config, { configType }) => {
-    if (config?.module?.rules) {
-      config.module.rules.push({
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "../"),
-      })
-    }
-
-    return config
   },
 }
 export default config
